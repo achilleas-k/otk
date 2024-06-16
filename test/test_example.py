039b5f488a2e1335186cb35397ee35dce55e5462
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-16 18:29:33 +0200

    bring over tests and adjust

---
+++ test/test_example.py
@@ -0,0 +1,13 @@
+import argparse
+import json
+import pathlib
+
+import pytest
+from otk.main import process_include
+
+
+@pytest.mark.parametrize("src_yaml", (pathlib.Path(__file__).parent / "data/base").glob("*.yaml"))
+def test_command_compile_on_base_examples(tmp_path, src_yaml):
+    expected = json.load(src_yaml.with_suffix(".json").open())
+    actual = process_include(src_yaml, {})
+    assert expected == actual
