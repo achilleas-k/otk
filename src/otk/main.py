3228a1c8e2ece6f6f539e4a09d1b1b00f7a984b0
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-16 18:39:59 +0200

    Keep tracking of working directory for relative include paths

--- src/otk/main.py
+++ src/otk/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from typing import Any
 
@@ -14,7 +15,7 @@ def replace_define(value, defines):
     return value
 
 
-def process_dict(data, defines):
+def process_dict(data, defines, cwd):
     """
     Dictionaries are iterated through and both the keys and values are processed.
     Keys define how a value is interpreted:
@@ -26,7 +27,7 @@ def process_dict(data, defines):
         if k.startswith("otk.include"):
             print(f"Loading {v}")
             del data[k]
-            data.update(process_include(v, defines))
+            data.update(process_include(v, defines, cwd))
         elif k.startswith("otk.define"):
             print(f"Defining {v}")
             defines.update(v)
@@ -35,36 +36,37 @@ def process_dict(data, defines):
             print(f"Dropping {k}")
             del data[k]
         else:
-            data[k] = process_value(v, defines)
+            data[k] = process_value(v, defines, cwd)
 
     return data
 
 
-def process_list(data, defines):
+def process_list(data, defines, cwd):
     """
     Process each value in a list.
     """
     for idx, item in enumerate(data.copy()):
-        data[idx] = process_value(item, defines)
+        data[idx] = process_value(item, defines, cwd)
     return data
 
 
-def process_include(path: str, defines: dict) -> dict:
+def process_include(path: str, defines: dict, cwd: str) -> dict:
     """
     Load a yaml file and send it to process_value() for processing.
     """
+    path = os.path.join(cwd, path)
     with open(path, mode="r", encoding="utf=8") as fp:
-        return process_value(yaml.safe_load(fp), defines)
+        return process_value(yaml.safe_load(fp), defines, cwd=os.path.dirname(path))
 
 
-def process_value(data, defines):
+def process_value(data, defines, cwd):
     """
     Process a dictionary value based on its type.
     """
     if isinstance(data, dict):
-        return process_dict(data, defines)
+        return process_dict(data, defines, cwd)
     if isinstance(data, list):
-        return process_list(data, defines)
+        return process_list(data, defines, cwd)
     if isinstance(data, str):
         data = replace_define(data, defines)
     return data
@@ -75,7 +77,7 @@ def main():
     defines: dict[str, Any] = {}
 
     # Treat the entrypoint as an include
-    data = process_include(path, defines)
+    data = process_include(path, defines, cwd=".")
 
     print(defines)
     print("---")
