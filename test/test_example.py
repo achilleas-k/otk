5cb11ef420c2b4f0fc70c27529a5792fb7b9f2a9
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-16 20:07:49 +0200

    Add error tests
    Add a test that produces an error with an expected message.  For now it
    only tests that a file not found error includes the name of the source
    file in the message.

--- test/test_example.py
+++ test/test_example.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import pathlib
 
@@ -11,3 +10,11 @@ def test_command_compile_on_base_examples(tmp_path, src_yaml):
     expected = json.load(src_yaml.with_suffix(".json").open())
     actual = process_include(src_yaml, {}, ".")
     assert expected == actual
+
+
+@pytest.mark.parametrize("src_yaml", (pathlib.Path(__file__).parent / "data/error").glob("*.yaml"))
+def test_errors(tmp_path, src_yaml):
+    expected = src_yaml.with_suffix(".err").read_text().strip()
+    with pytest.raises(FileNotFoundError) as exception:
+        process_include(src_yaml, {}, ".")
+    assert expected in str(exception.value)
