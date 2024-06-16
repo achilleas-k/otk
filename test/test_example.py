d5c7526af8afa63cc4ca060f45e9e668a7afcc56
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-16 18:40:57 +0200

    Add cwd to test_example

--- test/test_example.py
+++ test/test_example.py
@@ -9,5 +9,5 @@ from otk.main import process_include
 @pytest.mark.parametrize("src_yaml", (pathlib.Path(__file__).parent / "data/base").glob("*.yaml"))
 def test_command_compile_on_base_examples(tmp_path, src_yaml):
     expected = json.load(src_yaml.with_suffix(".json").open())
-    actual = process_include(src_yaml, {})
+    actual = process_include(src_yaml, {}, ".")
     assert expected == actual
