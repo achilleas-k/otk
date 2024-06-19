23fe4e4815aa3f043dca4281919fff7ddc50e947
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-19 22:56:15 +0200

    test: error test for undefined value

--- test/test_example.py
+++ test/test_example.py
@@ -15,6 +15,6 @@ def test_command_compile_on_base_examples(src_yaml):
 @pytest.mark.parametrize("src_yaml", (pathlib.Path(__file__).parent / "data/error").glob("*.yaml"))
 def test_errors(src_yaml):
     expected = src_yaml.with_suffix(".err").read_text().strip()
-    with pytest.raises(FileNotFoundError) as exception:
+    with pytest.raises(Exception) as exception:
         process_include(src_yaml, {})
     assert expected in str(exception.value)
